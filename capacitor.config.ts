796d739666164ed63625fbbe8c34271b40758a74
twoballs-ai <twoballs20@gmail.com>
2026-02-16 20:14:45 +0300

    fix: add gitignore and remove ignored files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ru.lost_jump.app',
+  appName: 'lost_jump',
+  webDir: 'dist'
+};
+
+export default config;
